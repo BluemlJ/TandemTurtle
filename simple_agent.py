e2e5d763dbda12c10952f46b5689683ec81ca109
Lin <lin-in-darmstadt+git@mailbox.org>
2019-05-22 12:37:02 +0200

    added comments to illuminate functionaliy, commented out unnecessary part of evaluate_leaf function.

--- simple_agent.py
+++ simple_agent.py
@@ -19,7 +19,7 @@ class Simple_Agent():
     # name - agent name
     # state size
     # action size
-    # number of mcts simulations
+    # number of MCTS simulations
     # cpuct - exploration coefficient for uct
     # model - the neural net. Not used in simple agent, but kept here for the purpose of later extension
     ##########
@@ -42,43 +42,54 @@ class Simple_Agent():
     ##########
     def simulate(self):
 
-
         lg.logger_mcts.info('ROOT NODE...%s', self.mcts.root.state.id)
-        self.mcts.root.state.render(lg.logger_mcts)
+        self.mcts.root.state.render(lg.logger_mcts) # log game state
         lg.logger_mcts.info('CURRENT PLAYER...%d', self.mcts.root.state.playerTurn)
 
         ##### MOVE THE LEAF NODE
         leaf, value, done, breadcrumbs = self.mcts.move_to_leaf()
-        # leaf.state.render(lg.logger_mcts)
+        leaf.state.render(lg.logger_mcts)
 
         ##### EVALUATE THE LEAF NODE
         value, breadcrumbs = self.evaluate_leaf(leaf, value, done, breadcrumbs)
 
         ##### BACKFILL THE VALUE THROUGH THE TREE
         self.mcts.back_fill(leaf, value, breadcrumbs)
+    ####
+    # act - run simulations updating the MC-search-tree. Then pick an action.
+    # param:
+    # state - the game state
+    # tau - 1 (in the beginning, when the moves are not yet deterministic)
+    #       or 0 (after some time, when the simple_agent starts playing deterministically.
+    # returns:
+    # action - the chosen action,
+    # pi - the priorities (?) of the different actions,
+    # value - (?)
+    # nn_value - zero in this simple case
+    # .
 
     def act(self, state, tau):
-
-
+        print("act with tau = %d",tau)
+        #### go to mcts node that corresponds to state or build new mcts.
         if self.mcts == None or state.id not in self.mcts.tree:
             self.build_mcts(state)
         else:
-            self.change_root_mcts(state)#so that it doesnt have to be searched again.
+            self.change_root_mcts(state)#so that it can use previous simulations
 
         #### run the simulation
-        for sim in range(self.MCTSsimulations): #TODO use fixed time instead of fixed nr of simulations
+        for sim in range(self.MCTSsimulations): #TODO (later) use fixed time instead of fixed nr of simulations
             lg.logger_mcts.info('***************************')
             lg.logger_mcts.info('****** SIMULATION %d ******', sim + 1)
             lg.logger_mcts.info('***************************')
             self.simulate() #updates MCTS
 
         #### get action values
-        pi, values = self.get_action_value(1)
+        pi, values = self.get_action_values(1)
 
-        ####pick the action
+        #### pick the action
         action, value = self.choose_action(pi, values, tau)
 
-        nextState, _, _ = state.take_action(action)
+        #nextState, _, _ = state.take_action(action) #only needed for nn_value
 
         #NN_value = -self.get_preds(nextState)[0]
         nn_value = 0# The Neural Net is not used yet anyway. Therefore the value is set to neutral.
@@ -90,23 +101,27 @@ class Simple_Agent():
 
         return (action, pi, value, nn_value)
 
-    def evaluate_leaf(self, leaf, value, done, breadcrumbs):
+    ####
+    # evaluate_leaf: .
+    def evaluate_leaf(self, leaf, eval_value, done, breadcrumbs):#TODO: delete breadcrumbs, its not used, is it?
 
         lg.logger_mcts.info('------EVALUATING LEAF------')
         if done == 0:
 
             #value, probs, allowedActions = self.get_preds(leaf.state)
-            allowedActions = np.array(leaf.state.allowedActions)
-            print(allowedActions)
+            '''allowedActions = np.array(leaf.state.allowedActions) # no rollouts. use static eval fct instead
+            print(allowedActions)'''
             # In first run leaf is empty so set value to fixed value
             if leaf.edges:
                 parent_edge = leaf.edges[0]# TODO: is the first edge of a node really the parent edge? Easier Alternative: use absolute evaluation fctn for the value.
-                value = eval.eval_move(parent_edge.action, parent_edge.inNode)
+                eval_value = eval.eval_move(parent_edge.action, parent_edge.inNode)
             else:
-                value = 100     # TODO what value?
-            lg.logger_mcts.info('PREDICTED VALUE FOR %d: %f', leaf.state.playerTurn, value)
+                eval_value = 100     # TODO (later) what value?
+            lg.logger_mcts.info('PREDICTED VALUE FOR %d: %f', leaf.state.playerTurn, eval_value)
 
-            print(allowedActions.shape)
+            # no rollouts. use static eval fctn
+            '''
+            print("allowedActions.shape = ", allowedActions.shape)
             probs = np.ones(allowedActions.shape[0])#TODO: is this the right data type? array?
             #probs = probs[allowedActions]
 
@@ -122,13 +137,13 @@ class Simple_Agent():
 
                 newEdge = mc.Edge(leaf, node, probs[idx], action)
                 leaf.edges.append((action, newEdge))
+        '''
+        else:#after evaluation is done
+            lg.logger_mcts.info('GAME VALUE FOR %d: %f', leaf.playerTurn, eval_value)
 
-        else:
-            lg.logger_mcts.info('GAME VALUE FOR %d: %f', leaf.playerTurn, value)
+        return (eval_value, breadcrumbs)
 
-        return (value, breadcrumbs)
-
-    def get_action_value(self, tau):
+    def get_action_values(self, tau):
         print("Tau: ", tau)
 
         edges = self.mcts.root.edges
