f83e8e334a994fa15096e82862b6c166f60798cd
Lin <lin-in-darmstadt+git@mailbox.org>
2019-05-29 13:23:05 +0200

    first draft of Neural Net that learns only from bpgn data

---
+++ training_from_database/config_training.py
@@ -0,0 +1,22 @@
+
+BATCH_SIZE = 256
+EPOCHS = 1
+REG_CONST = 0.0001
+LEARNING_RATE = 0.1
+MOMENTUM = 0.9
+TRAINING_LOOPS = 10
+INPUT_DIM = [34,8,8]  # TODO: adjust to real input dim
+OUTPUT_DIM_VALUE_HEAD = 1
+
+HIDDEN_CNN_LAYERS = [
+	{'filters':75, 'kernel_size': (4,4)}
+	 , {'filters':75, 'kernel_size': (4,4)}
+	 , {'filters':75, 'kernel_size': (4,4)}
+	 , {'filters':75, 'kernel_size': (4,4)}
+	 , {'filters':75, 'kernel_size': (4,4)}
+	 , {'filters':75, 'kernel_size': (4,4)}
+	]
+
+#### EVALUATION
+EVAL_EPISODES = 20
+SCORING_THRESHOLD = 1.3
