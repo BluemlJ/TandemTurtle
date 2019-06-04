6d21d4b00ab4209f8361ac1f9e78a3d393a44434
flon <florian.netzer@arcor.de>
2019-06-04 12:29:41 +0200

    train/test/validation split

--- databaseTraining/parse_data.py
+++ databaseTraining/parse_data.py
@@ -6,48 +6,95 @@ results.txt one line for each position. -1 if black will win the game, 1 white, 
 next_move next move for each position as string with board number (e.g B1 e2e4)
 
 TODO How long does this take to finish??? make faster
-TODO  add train/test/validation split
 TODO filter games (cancelled, connection lost)
 """
-from chess.variant import BughouseBoards
 import sys
+
 sys.path.append("..")
-from database_extraction.extract import read_data
+
 import chess.pgn
-from input_representation import *
 import os
 import codecs
 
-path = "../database_extraction/data/"
-files = []
-# r=root, d=directories, f = files
-for r, d, f in os.walk(path):
-    for file in f:
-        if '.bpgn' in file and ".bz2" not in file:
-            files.append(os.path.join(r, file))
-with open("data/all_boards.txt", "w") as all_boards, open("data/results.txt", "w") as labels, open("data/next_move.txt",
-                                                                                                   'w') as next_moves:
-    for file in files:
-        with codecs.open(file, encoding="utf-8-sig", errors='ignore') as f:
-            game = chess.pgn.read_game(f)
-            while game:
-                boards = game.board()
-                result = game.headers["Result"]
-                if result == '0-1':
-                    label = "-1"
-                elif result == '1-0':
-                    label = "1"
-                else:
-                    label = "0"
 
-                for move in game.mainline_moves():
+def parse_all_positions(in_dir="../database_extraction/data/", out_dir="data/"):
+    files = []
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(in_dir):
+        for file in f:
+            if '.bpgn' in file and ".bz2" not in file:
+                files.append(os.path.join(r, file))
+    with open(out_dir + "all_boards.txt", "w") as all_boards, \
+            open(out_dir + "results.txt", "w") as labels, \
+            open(out_dir + "next_move.txt", 'w') as next_moves:
+
+        for file in files:
+            with codecs.open(file, encoding="utf-8-sig", errors='ignore') as f:
+                game = chess.pgn.read_game(f)
+                while game:
+                    boards = game.board()
+                    result = game.headers["Result"]
+                    if result == '0-1':
+                        label = "-1"
+                    elif result == '1-0':
+                        label = "1"
+                    else:
+                        label = "0"
+
+                    for move in game.mainline_moves():
+                        all_boards.write(boards.fen + "\n")
+                        labels.write(label + "\n")
+                        next_moves.write(str(move) + "\n")
+                        boards.push(move)
+
                     all_boards.write(boards.fen + "\n")
                     labels.write(label + "\n")
-                    next_moves.write(str(move) + "\n")
-                    boards.push(move)
+                    next_moves.write("end\n")
 
-                all_boards.write(boards.fen + "\n")
-                labels.write(label + "\n")
-                next_moves.write("end\n")
+                    game = chess.pgn.read_game(f)
 
-                game = chess.pgn.read_game(f)
+
+
+
+def splits(dir="data/"):
+    """
+    Splits data into train/validation/tset file
+    TODO make split size parameters
+    :param dir: data dir
+    """
+    with open(dir + "all_boards.txt", "r") as all_boards, \
+            open(dir + "results.txt", "r") as labels, \
+            open(dir + "next_move.txt", 'r') as next_moves,\
+            open(dir + "position.train",'w') as p_train,\
+            open(dir + "position.validation",'w') as p_validation,\
+            open(dir + "position.test",'w') as p_test,\
+            open(dir + "result.train",'w') as v_train,\
+            open(dir + "result.validation",'w') as v_validation,\
+            open(dir + "result.test",'w') as v_test, \
+            open(dir + "nm.train",'w') as nm_train, \
+            open(dir + "nm.validation",'w') as nm_validation, \
+            open(dir + "nm.test",'w') as nm_test:\
+
+        #shuffle_files(["all_boards.txt","results.txt","next_move.txt"]) TODO
+
+        for i,(position,result,nm) in enumerate(zip(all_boards,labels,next_moves)):
+            #every tenth point as test
+            if i%10 == 0:
+                p_test.write(position)
+                v_test.write(result)
+                nm_test.write(nm)
+            elif i%15 == 0:
+                p_validation.write(position)
+                v_validation.write(result)
+                nm_validation.write(nm)
+            else:
+                p_train.write(position)
+                v_train.write(result)
+                nm_train.write(nm)
+
+#TODO shuffle lines in files so that the lines still match the lines in the other files
+def shuffle_files(files):
+    raise NotImplementedError
+
+#parse_all_positions()
+splits()
