80457c3959053f9f63124f37e62c6e4f00e4ac85
Maxi Laptop <Maximiai.gmail.com>
2019-06-17 15:50:57 +0200

    added changes to config

--- databaseTraining/config_training.py
+++ databaseTraining/config_training.py
@@ -4,7 +4,10 @@ EPOCHS = 1
 REG_CONST = 0.0001
 LEARNING_RATE = 0.1
 MOMENTUM = 0.9
-NR_RESIDUAL_LAYERS = 40
+NR_RESIDUAL_LAYERS = 1     # TODO change, was 40 moved down for testing on low memory
 KERNEL_SIZE_CONVOLUTION = 3
 NR_CONV_FILTERS = 256
 SIZE_VALUE_HEAD_HIDDEN = 256
+TEST_MODE = True
+# Set to "" if not in google colab
+GDRIVE_FOLDER = "" # ""drive/My Drive/KI_Praktikum/"
