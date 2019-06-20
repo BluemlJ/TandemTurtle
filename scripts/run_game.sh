5d90e1e30f1b900833035c331773d82b33a6d55b
Lin <lin-in-darmstadt+git@mailbox.org>
2019-06-20 16:29:31 +0200

    added permission fix to run_game script

--- scripts/run_game.sh
+++ scripts/run_game.sh
@@ -5,3 +5,4 @@ chmod 755 sleep15_and_main.sh
 cd ../
 cd ../tinyChessServer/
 sudo xterm -e sudo node index.js & xterm -e ../TandemTurtle/scripts/sleep5_and_run_serve.sh & xterm -e ../TandemTurtle/scripts/sleep15_and_main.sh
+sudo chmod 777 chess/
