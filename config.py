d1164153163ed57852dd24f5e86c881531f9b1a0
Jannis Blueml <me@jblue.ml>
2019-06-25 10:49:56 +0200

    add selfplay part1, missing playMatches

--- config.py
+++ config.py
@@ -5,7 +5,8 @@
 INITIAL_RUN_NUMBER = None
 INITIAL_MODEL_VERSION = None
 INITIAL_MEMORY_VERSION = None
-INITIAL_MODEL_PATH = ""  # ""/run/models/simple_model_v1"
+INITIAL_MODEL_PATH = ""
+PRETRAINED_MODEL_PATH = "/run/models/simple_model_v1"
 
 # SELF PLAY
 EPISODES = 30
@@ -14,9 +15,7 @@ MEMORY_SIZE = 30000
 TURNS_WITH_HIGH_NOISE = 10  # turn when the agent starts playing with less noise (less exploration)
 CPUCT = 1.41
 
-"""
-
-#### RETRAINING
+# RETRAINING
 BATCH_SIZE = 256
 EPOCHS = 1
 REG_CONST = 0.0001
@@ -24,20 +23,9 @@ LEARNING_RATE = 0.1
 MOMENTUM = 0.9
 TRAINING_LOOPS = 10
 
-HIDDEN_CNN_LAYERS = [
-	{'filters':75, 'kernel_size': (4,4)}
-	 , {'filters':75, 'kernel_size': (4,4)}
-	 , {'filters':75, 'kernel_size': (4,4)}
-	 , {'filters':75, 'kernel_size': (4,4)}
-	 , {'filters':75, 'kernel_size': (4,4)}
-	 , {'filters':75, 'kernel_size': (4,4)}
-	]
-
-#### EVALUATION
+# EVALUATION
 EVAL_EPISODES = 20
 SCORING_THRESHOLD = 1.3
-
-"""
 
 run_folder = './run/'
 run_archive_folder = './run/archive/'
