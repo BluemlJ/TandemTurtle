949653bbd836f55fbd827967584f660d10a8ba0d
Jannis Blueml <me@jblue.ml>
2019-06-27 10:07:51 +0200

    new TODO, commit env to memory

--- util/memory.py
+++ util/memory.py
@@ -14,11 +14,9 @@ class Memory:
         self.ltmemory = deque(maxlen=config.MEMORY_SIZE)
         self.stmemory = deque(maxlen=config.MEMORY_SIZE)
 
-    def commit_stmemory(self, identities, state, actionValues):
-        for r in identities(state, actionValues):
-            self.stmemory.append({
-                'board': r[0].board, 'state': r[0], 'id': r[0].id, 'AV': r[1], 'playerTurn': r[0].playerTurn
-            })
+    def commit_stmemory(self, env, state):
+        # TODO commit the env with board and mcts to the memory
+        pass
 
     def commit_ltmemory(self):
         for i in self.stmemory:
