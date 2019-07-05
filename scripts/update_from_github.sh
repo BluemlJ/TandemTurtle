1d015287d5abefb745af7f62e70ca818d7cd1d0a
Lin <lin-in-darmstadt+git@mailbox.org>
2019-07-05 16:32:20 +0200

    added copying chess folder to backend that was missing.

--- scripts/update_from_github.sh
+++ scripts/update_from_github.sh
@@ -4,14 +4,13 @@ echo "----------Updating TandemTurtle"
 cd ../
 git pull
 
-echo "----------Removing chess folder"
+echo "----------Removing chess folder in TandemTurtle"
 rm -r chess #new chess folder comes from python chess (see below)
 
 echo "----------Updating Python Chess"
 cd ../python-chess
 git pull
-echo "----------Copying chess folder"
-cp -r chess ../TandemTurtle
+
 
 echo "----------Updating server"
 cd ../tinyChessServer
@@ -22,8 +21,17 @@ then
       npm install
       cd vue-frontend/
       npm install
+      cd ..
 else
       echo "Already up to date."
 fi
 
+echo "----------Removing chess folder in TandemTurtle"
+rm -r backend/chess #new chess folder comes from python chess (see below)
+
+
+echo "----------Copying chess folder"
+cd ../python-chess
+cp -r chess ../TandemTurtle
+cp -r chess ../tinyChessServer/backend/
 
